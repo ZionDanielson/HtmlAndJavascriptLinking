c4f8f677a56e59e416551e20487d40b9af4161c4
Zion Danielson <zionsdocs@gmail.com>
2023-04-27 15:32:41 -0400

    index.js

--- scripts/index.js
+++ scripts/index.js
@@ -14,11 +14,36 @@ const greeting = document.getElementById("greeting");
 // has a value property with it's value.
 const nameTextbox = document.getElementById("nameTextbox");
 
-//pressMeButton is a button element, it will raise an
+// pressMeButton is a button element, it will raise an
 // "onclicked" event that I want it to respond to.
 const pressMeButton = document.getElementById("pressMeButton")
 
 
+function init(){
+    console.log ("hello from inside the init function!")
+    //code inside the init here will run
+    // when the page is finished initiializing.
+
+    // it does so because of the window.onload line below...
+    
+    onPressMeButtonClicked.onclick = onPressMeButtononClicked;
+
+
+    // This line of code will wire up the init function to exp
+    //when the page is done loading
+    window.onload = init;
+
+    function onPressMeButtonClicked(){
+        // code inside this function should execute when the button is clicked
+        console.log("hello from inside the onPressMeButtonClicked() function!")
+        
+        let username = nameTextbox.value;
+
+
+        greeting.innerHTML = `Hello to ${username} from inside the onPressMeButtonClicked`
+
+    }
+}
 
 
 console.log("the index.js has finished executing...");
