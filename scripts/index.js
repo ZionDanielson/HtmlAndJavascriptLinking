4bc2064f28dbb646d3a16549a72a88881ac6bde7
Zion Danielson <zionsdocs@gmail.com>
2023-04-27 15:21:17 -0400

    index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,8 @@
 "use strict";
 
+console.log("the index.js has begun executing...");
+
+
 // Here I declare all of the variables that I will need
 // to hold element objects from the HTML page.
 // This will let me interact with item.
@@ -7,6 +10,15 @@
 // greeting is a DIV tag, and I can change it's .innerHTML
 const greeting = document.getElementById("greeting");
 
-//name
-const nameTextbox = document.getElementById("nameTextbox")
+// nameTextbox is an INPTU tag of type textbox, and it
+// has a value property with it's value.
+const nameTextbox = document.getElementById("nameTextbox");
+
+//pressMeButton is a button element, it will raise an
+// "onclicked" event that I want it to respond to.
 const pressMeButton = document.getElementById("pressMeButton")
+
+
+
+
+console.log("the index.js has finished executing...");
